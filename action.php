e7672c51d99a0805b887a259ecac5614fb05ffa9
juliusverrel <juliusverrel@users.noreply.github.com>
2019-06-15 23:08:35 +0200

    Update action.php

--- action.php
+++ action.php
@@ -30,7 +30,7 @@ class action_plugin_pagetemplater extends DokuWiki_Action_Plugin {
      * Register the eventhandlers.
      */
     function register(Doku_Event_Handler $controller) {
-        $controller->register_hook('RENDERER_CONTENT_POSTPROCESS', 'BEFORE', $this, 'handle_content_display', array ());
+        $controller->register_hook('RENDERER_CONTENT_POSTPROCESS', 'AFTER', $this, 'handle_content_display', array ());
         $controller->register_hook('PARSER_METADATA_RENDER', 'AFTER', $this, 'handle_meta_data', array ());
     }
 
